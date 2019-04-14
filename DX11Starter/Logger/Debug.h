50a98eb8001fa810cd658b15932b07f548992a8b
Shantanu Jamble <sj6045@rit.edu>
2019-04-14 14:37:31 -0400

    Added usage comment.

--- DX11Starter/Logger/Debug.h
+++ DX11Starter/Logger/Debug.h
@@ -3,6 +3,20 @@
 #include <vector>
 #include <atomic>
 
+/*
+	Debug is a singleton class which you can use to print logs in file or on console string.
+	Log method has a default parameter which decides where to put logs. By default it is set to false.
+	if flag is false -> print to file
+	else if flag is true ->print to console
+	Usage:
+		To print logs on screen,
+		Debug::Log("log statement",true);
+
+		To print in the file
+		Debug::Log("log statement");
+*/
+
+
 class Debug
 {
 private:
