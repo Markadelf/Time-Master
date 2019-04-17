da8fa104d91f21c35c35e3a553e1a1836378c4a6
Mark DelFavero <markadelf@gmail.com>
2019-04-16 21:09:31 -0400

    Basic shadow map
    A single shadow map on a directional light

--- DX11Starter/RenderingCore/Renderer.cpp
+++ DX11Starter/RenderingCore/Renderer.cpp
@@ -45,8 +45,15 @@ Renderer::~Renderer()
 {
 	delete m_ps;
 	delete m_vs;
+	delete m_shadowVS;
 
 	m_sampler->Release();
+
+	m_shadowSampler->Release();
+	m_shadowRasterizer->Release();
+
+	m_shadowDSV->Release();
+	m_shadowSRV->Release();
 }
 
 // --------------------------------------------------------
@@ -60,6 +67,7 @@ void Renderer::Init()
 	// Essentially: "What kind of shape should the GPU draw with our data?"
 	context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 	InitializeShaders();
+	InitializeShadowMaps();
 	resize(width, height);
 }
 
@@ -83,6 +91,73 @@ void Renderer::InitializeShaders()
 
 	m_ps = new SimplePixelShader(device, context);
 	m_ps->LoadShaderFile(L"PixelShader.cso");
+
+	m_shadowVS = new SimpleVertexShader(device, context);
+	m_shadowVS->LoadShaderFile(L"ShadowVS.cso");
+}
+
+void Renderer::InitializeShadowMaps()
+{
+	// Create shadow requirements ------------------------------------------
+	shadowMapSize = 1024;
+
+	// Create the actual texture that will be the shadow map
+	D3D11_TEXTURE2D_DESC shadowDesc = {};
+	shadowDesc.Width = shadowMapSize;
+	shadowDesc.Height = shadowMapSize;
+	shadowDesc.ArraySize = 1;
+	shadowDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL | D3D11_BIND_SHADER_RESOURCE;
+	shadowDesc.CPUAccessFlags = 0;
+	shadowDesc.Format = DXGI_FORMAT_R32_TYPELESS;
+	shadowDesc.MipLevels = 1;
+	shadowDesc.MiscFlags = 0;
+	shadowDesc.SampleDesc.Count = 1;
+	shadowDesc.SampleDesc.Quality = 0;
+	shadowDesc.Usage = D3D11_USAGE_DEFAULT;
+	ID3D11Texture2D* shadowTexture;
+	device->CreateTexture2D(&shadowDesc, 0, &shadowTexture);
+
+	// Create the depth/stencil
+	D3D11_DEPTH_STENCIL_VIEW_DESC shadowDSDesc = {};
+	shadowDSDesc.Format = DXGI_FORMAT_D32_FLOAT;
+	shadowDSDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	shadowDSDesc.Texture2D.MipSlice = 0;
+	device->CreateDepthStencilView(shadowTexture, &shadowDSDesc, &m_shadowDSV);
+
+	// Create the SRV for the shadow map
+	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+	srvDesc.Format = DXGI_FORMAT_R32_FLOAT;
+	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	srvDesc.Texture2D.MipLevels = 1;
+	srvDesc.Texture2D.MostDetailedMip = 0;
+	device->CreateShaderResourceView(shadowTexture, &srvDesc, &m_shadowSRV);
+
+	// Release the texture reference since we don't need it
+	shadowTexture->Release();
+
+	// Create the special "comparison" sampler state for shadows
+	D3D11_SAMPLER_DESC shadowSampDesc = {};
+	shadowSampDesc.Filter = D3D11_FILTER_COMPARISON_MIN_MAG_MIP_LINEAR; // Could be anisotropic
+	shadowSampDesc.ComparisonFunc = D3D11_COMPARISON_LESS;
+	shadowSampDesc.AddressU = D3D11_TEXTURE_ADDRESS_BORDER;
+	shadowSampDesc.AddressV = D3D11_TEXTURE_ADDRESS_BORDER;
+	shadowSampDesc.AddressW = D3D11_TEXTURE_ADDRESS_BORDER;
+	shadowSampDesc.BorderColor[0] = 1.0f;
+	shadowSampDesc.BorderColor[1] = 1.0f;
+	shadowSampDesc.BorderColor[2] = 1.0f;
+	shadowSampDesc.BorderColor[3] = 1.0f;
+	device->CreateSamplerState(&shadowSampDesc, &m_shadowSampler);
+
+	// Create a rasterizer state
+	D3D11_RASTERIZER_DESC shadowRastDesc = {};
+	shadowRastDesc.FillMode = D3D11_FILL_SOLID;
+	shadowRastDesc.CullMode = D3D11_CULL_BACK;
+	shadowRastDesc.DepthClipEnable = true;
+	shadowRastDesc.DepthBias = 1000; // Multiplied by (smallest possible value > 0 in depth buffer)
+	shadowRastDesc.DepthBiasClamp = 0.0f;
+	shadowRastDesc.SlopeScaledDepthBias = 1.0f;
+	device->CreateRasterizerState(&shadowRastDesc, &m_shadowRasterizer);
+
 }
 
 void Renderer::Begin()
@@ -113,6 +188,58 @@ void Renderer::End()
 
 void Renderer::RenderGroup(DrawGroup& drawGroup)
 {
+	// Create the view and projection for the shadow map light
+	XMMATRIX shView = XMMatrixLookToLH(
+		XMVectorSet(10, 10, 0, 0),
+		XMLoadFloat3(&drawGroup.m_lightList[0].Direction),
+		XMVectorSet(0, 1, 0, 0));
+	XMStoreFloat4x4(&m_shadowViewMatrix, XMMatrixTranspose(shView));
+
+	XMMATRIX shProj = XMMatrixOrthographicLH(
+		10,
+		10,
+		0.1f,
+		50);
+	XMStoreFloat4x4(&m_shadowProjectionMatrix, XMMatrixTranspose(shProj));
+
+	// Set up the initial pipeline state for shadow map creation
+	context->OMSetRenderTargets(0, 0, m_shadowDSV);
+	context->ClearDepthStencilView(m_shadowDSV, D3D11_CLEAR_DEPTH, 1.0f, 0);
+	context->RSSetState(m_shadowRasterizer);
+
+	// Update the viewport to describe rendering into
+	// the full shadow map resolution
+	D3D11_VIEWPORT vp = {};
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	vp.Width = (float)shadowMapSize;
+	vp.Height = (float)shadowMapSize;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	context->RSSetViewports(1, &vp);
+
+	// Set up shaders for rendering
+	m_shadowVS->SetShader();
+	m_shadowVS->SetMatrix4x4("view", m_shadowViewMatrix);
+	m_shadowVS->SetMatrix4x4("projection", m_shadowProjectionMatrix);
+
+	// Turn OFF the pixel shader
+	context->PSSetShader(0, 0, 0);
+
+	// Render to the shadow map
+	for (size_t i = 0; i < drawGroup.m_visibleCount; i++)
+	{
+		Mesh* mesh = AssetManager::get().GetMeshPointer(drawGroup.m_opaqueObjects[i].GetMeshHandle());
+		RenderToShadowMap(drawGroup.m_opaqueObjects[i].GetTransform(), mesh);
+	}
+
+	context->OMSetRenderTargets(1, &backBufferRTV, depthStencilView);
+	vp.Width = (float)width;
+	vp.Height = (float)height;
+	context->RSSetViewports(1, &vp);
+	context->RSSetState(0);
+
+	// Render the scene
 	for (size_t i = 0; i < drawGroup.m_visibleCount; i++)
 	{
 		RenderVisibleEntity(drawGroup.m_opaqueObjects[i], drawGroup.m_camera, drawGroup.m_lightList, drawGroup.m_lightCount);
@@ -129,6 +256,8 @@ void Renderer::Render(SimplePixelShader* ps, SimpleVertexShader* vs, Material* m
 	vs->SetMatrix4x4("world", transform);
 	vs->SetMatrix4x4("view", camera.GetView());
 	vs->SetMatrix4x4("projection", camera.GetProjection());
+	vs->SetMatrix4x4("shadowView", m_shadowViewMatrix);
+	vs->SetMatrix4x4("shadowProj", m_shadowProjectionMatrix);
 
 	// Once you've set all of the data you care to change for
 	// the next draw call, you need to actually send it to the GPU
@@ -140,7 +269,9 @@ void Renderer::Render(SimplePixelShader* ps, SimpleVertexShader* vs, Material* m
 	// Only copies first ten as the size is fixed on the shader. Subtracting the pad value is necessary because the 
 	ps->SetShaderResourceView("diffuseTexture", *AssetManager::get().GetTexturePointer(mat->GetDiffuseTextureHandle()));
 	ps->SetShaderResourceView("roughnessTexture", *AssetManager::get().GetTexturePointer(mat->GetRoughnessTextureHandle()));
+	ps->SetShaderResourceView("ShadowMap", m_shadowSRV);
 	ps->SetSamplerState("basicSampler", sampler);
+	ps->SetSamplerState("ShadowSampler", m_shadowSampler);
 	ps->CopyAllBufferData();
 
 	// Set the vertex and pixel shaders to use for the next Draw() command
@@ -169,6 +300,27 @@ void Renderer::Render(SimplePixelShader* ps, SimpleVertexShader* vs, Material* m
 		mesh->GetIndexCount(),     // The number of indices to use (we could draw a subset if we wanted)
 		0,     // Offset to the first index we want to use
 		0);    // Offset to add to each index when looking up vertices
+}
+
+void Renderer::RenderToShadowMap(DirectX::XMFLOAT4X4& transform, Mesh* mesh)
+{
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+
+	// Grab the data from the first entity's mesh
+	ID3D11Buffer* vb = mesh->GetVertexBuffer();
+	ID3D11Buffer* ib = mesh->GetIndexBuffer();
+
+	// Set buffers in the input assembler
+	context->IASetVertexBuffers(0, 1, &vb, &stride, &offset);
+	context->IASetIndexBuffer(ib, DXGI_FORMAT_R32_UINT, 0);
+
+	// Finalize vertex shader data
+	m_shadowVS->SetMatrix4x4("world", transform);
+	m_shadowVS->CopyAllBufferData();
+
+	// Finally do the actual drawing
+	context->DrawIndexed(mesh->GetIndexCount(), 0, 0);
 }
 
 void Renderer::RenderVisibleEntity(DrawItem& entity, Camera& camera, Light* lights, int lightCount)
--- DX11Starter/RenderingCore/Renderer.h
+++ DX11Starter/RenderingCore/Renderer.h
@@ -13,19 +13,39 @@
 class Renderer
 	: public DXCore
 {
+	// Shaders
 	SimplePixelShader* m_ps;
 	SimpleVertexShader* m_vs;
+	SimpleVertexShader* m_shadowVS;
+
+	// Smapler States
 	ID3D11SamplerState* m_sampler;
+	ID3D11SamplerState* m_shadowSampler;
+
+	// Rasterizer states
+	ID3D11RasterizerState* m_shadowRasterizer;
+
+	int shadowMapSize;
+
+	// Temporary shadow qualities
+	// TODO: Move somewhere else
+	ID3D11DepthStencilView* m_shadowDSV;
+	ID3D11ShaderResourceView* m_shadowSRV;
+	DirectX::XMFLOAT4X4 m_shadowViewMatrix;
+	DirectX::XMFLOAT4X4 m_shadowProjectionMatrix;
 
 public:
 	Renderer(HINSTANCE hInstance);
 	~Renderer();
 
+private:
 	// Overridden setup and game loop methods, which
 	// will be called automatically
 	void Init();
 	void InitializeShaders();
-	
+	void InitializeShadowMaps();
+
+public:
 	// Render Controls
 	void Begin();
 	void End();
@@ -36,6 +56,7 @@ public:
 private:
 	void RenderVisibleEntity(DrawItem& entity, Camera& camera, Light* lights, int lightCount);
 	void Render(SimplePixelShader* ps, SimpleVertexShader* vs, Material* mat, ID3D11SamplerState* sampler, DirectX::XMFLOAT4X4& transform, Mesh* mesh, Camera& camera, Light* lights, int lightCount);
+	void RenderToShadowMap(DirectX::XMFLOAT4X4& transform, Mesh* mesh);
 
 };
 
