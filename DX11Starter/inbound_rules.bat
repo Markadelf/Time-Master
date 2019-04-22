9536edafd7f17e8e9526077f4d6ad311a41a8ad8
Shantanu Jamble (RIT Student) <sj6045@ad.rit.edu>
2019-04-22 13:13:24 -0400

    Fixed batch file

--- DX11Starter/inbound_rules.bat
+++ DX11Starter/inbound_rules.bat
@@ -1,9 +1,15 @@
-ECHO OFF
+echo OFF
 set LOCALPORT=30000-30001
 set RULE_NAME="TimeMasters"
-set PROTOCOL="UDP"
-set DIR="IN"
-set ACTION="ALLOW"
+set PROTOCOL=udp
+set DIR=in
+set ACTION=allow
 
 
-echo netsh advfirewall add rule name =%RULE_NAME% dir=%DIR% localport=%LOCALPORT% action=%ACTION% protocol=%PROTOCOL%
+netsh advfirewall firewall show rule name=%RULE_NAME%
+
+if ERRORLEVEL 1 (
+netsh advfirewall firewall add rule name=%RULE_NAME% dir=%dir% protocol=%PROTOCOL% localport=%LOCALPORT% action=%ACTION%
+) else (
+    echo Rule is already present
+)
